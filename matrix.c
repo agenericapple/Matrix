6429d4f8063ef009e215c312af32766f4ff3f01c
GenericApple <103732419+agenericapple@users.noreply.github.com>
2023-12-13 14:25:44 +0100

    Update and rename demo_app.c to matrix.c

--- matrix.c
+++ matrix.c
@@ -5,8 +5,7 @@
 
 int32_t demo_app_app(void* p) {
     UNUSED(p);
-    FURI_LOG_I("TEST", "Hello world");
-    FURI_LOG_I("TEST", "I'm demo_app!");
+    FURI_LOG_I("TEST", "Running");
 
     return 0;
 }
